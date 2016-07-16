ddc91187294f811d0f609812576b2f3ab26de4c7
Gleb Lobastov <0000reg@gmail.com>
2016-07-16 14:12:23 +0500

    Отрефакторил и настроил события

--- finstat/static/finstat/js/interval/view.js
+++ finstat/static/finstat/js/interval/view.js
@@ -7,65 +7,198 @@ define([
    'text!finstat/interval/header.html',
    'text!finstat/interval/addRow.html',
    'text!finstat/interval/form.html',
-   'finstat/defaults',
+   'finstat/defaults'
 ], function (Backbone, _, moment, single, transactionTpl, headerTpl, addRow, formTpl, finstatDefaults) {
    var deps;
 
    var Transaction = Backbone.Model.extend({
-      initialize: function (attributes, options) {
-         this.alwaysNew = options && options.alwaysNew;
-         this.on('change', function () {
-            console.log('transaction ' + this.cid + ' changed');
-//            if (this.income > 0) {
-//               this.set('income', +this.get('amount'), {slient: true})
-//            } else {
-//               this.set('outcome', +this.get('amount'), {slient: true})
-//            }
-         }, this)
-      },
-      isNew: function () {
-         return this.alwaysNew || Backbone.Model.prototype.isNew.call(this);
-      },
+      _alwaysNew: false,
       defaults: {
          amount: 10,
          date: new Date().toISOString().substr(0, 10),
          account_from: null,
          account_to: null
+      },
+      urlRoot: 'api/transactions',
+      initialize: function (attributes, options) {
+         this._alwaysNew = options && options.alwaysNew;
+      },
+      isNew: function () {
+         return this._alwaysNew || Backbone.Model.prototype.isNew.call(this);
+      },
+      create: function (data) {
+         this.set(data);
+         this.save();
+      },
+      resolveAttributes: function () {
+         var
+            values = this.toJSON(),
+            resolved = {};
+         switch (values.transaction_type) {
+            case single.consts.TT_INCOME:
+               resolved.rowClass = 'finstat__bar-income';
+               break;
+            case single.consts.TT_OUTCOME:
+               resolved.rowClass = 'finstat__bar-outcome';
+               break;
+            default:
+               resolved.rowClass = '';
+         }
+         resolved.account_from = single.accounts.getName(values.fk_account_from);
+         resolved.account_to = single.accounts.getName(values.fk_account_to);
+         resolved.category = single.categories.getName(values.fk_category);
+         resolved.id = values.id;
+         resolved.amount = values.amount;
+         resolved.comment = values.comment;
+         return resolved;
+      },
+      sign: function () {
+         var transactionType = this.get('transaction_type');
+         if (transactionType === single.consts.TT_INCOME) {
+            return +1;
+         } else if (transactionType === single.consts.TT_OUTCOME) {
+            return -1
+         } else {
+            return 0
+         }
+      },
+      delta: function () {
+         return this.sign() * (this.previous('amount') - this.get('amount'));
+      },
+      cases: function (returnIfIncome, returnIfOutcome, returnElse) {
+         var sign = this.sign();
+         return sign ? (sign > 0 ? returnIfIncome : returnIfOutcome) : returnElse;
       }
    });
 
    var Transactions = Backbone.Collection.extend({
       url: 'api/transactions',
+      comparator: function (collection) {
+         return -collection.get('id');
+      },
       initialize: function () {
-         this.on('change', function () {
-            console.log('transactions ' + this.cid + ' changed');
-         }, this);
-         this.on('add', function () {
-            console.log('transactions ' + this.cid + ' updated');
-         }, this)
+      },
+      calcAmount: function (transaction_type) {
+         return this.reduce(function (total, currentItem) {
+            var matches = !transaction_type || currentItem.get('transaction_type') === transaction_type;
+            return total + (matches ? +currentItem.get('amount') : 0);
+         }, 0);
       },
       calcIncome: function () {
-         return this.reduce(function (total, currentItem) {
-            return total + (
-                  currentItem.get('transaction_type') === single.consts.TT_INCOME ? +currentItem.get('amount') : 0
-               );
-         }, 0);
+         return this.calcAmount(single.consts.TT_INCOME)
       },
       calcOutcome: function () {
-         return this.reduce(function (total, currentItem) {
-            return total + (
-                  currentItem.get('transaction_type') === single.consts.TT_OUTCOME ? +currentItem.get('amount') : 0
-               );
-         }, 0);
+         return this.calcAmount(single.consts.TT_OUTCOME)
       }
    });
 
-   var Interval = Backbone.Model.extend({});
+   var TransactionView = Backbone.View.extend({
+      className: "row finstat__show-on-hover_area finstat__highlight-row",
+      events: {
+         'click .finstat__remove-icon': "destroy"
+      },
+      template: _.template(transactionTpl),
+      dateSplitterTpl: _.template(headerTpl),
+      render: function () {
+         this.$el.html(this.template(this.model.resolveAttributes()));
+         deps.turnEditable.call(this);
+         return this;
+      },
+      destroy: function () {
+         this.model.destroy();
+         this.remove();
+      }
+   });
+
+   var TransactionsView = Backbone.View.extend({
+      initialize: function () {
+         var self = this;
+         this.collection.on('add', function (model) {
+            self.createElement.call(self, model)
+         });
+      },
+      render: function () {
+         this.collection.each(function (transaction) {
+            var transactionView = new TransactionView({
+               model: transaction,
+               header: this.header
+            });
+            this.$el.append(transactionView.render().el);
+         }, this);
+         return this;
+      },
+      createElement: function (transaction) {
+         var transactionView = new TransactionView({
+            model: transaction
+         });
+         this.$el.prepend(transactionView.render().el);
+      }
+   });
+
+   var TransactionFormView = Backbone.View.extend({
+      className: "form-inline",
+      events: {
+         'click .finstat__submit-icon': 'submit'
+      },
+      id: 'finstat__form-edit-transaction',
+      model: Transaction,
+      tagName: 'form',
+      template: _.template(formTpl),
+      initialize: function () {
+      },
+      render: function () {
+         this.$el.html(this.template({
+            amount: 100,
+            date: (new Date()).toISOString().substring(0, 10)
+         }));
+         return this;
+      },
+      collectFormData: function () {
+         return $('#finstat__form-edit-transaction').serializeArray().reduce(
+            function (attributes, item) {
+               attributes[item.name] = item.value;
+               return attributes;
+            }, {}
+         );
+      },
+      submit: function () {
+         this.model.create(this.collectFormData());
+      }
+   });
+
+
+   var Interval = Backbone.Model.extend({
+      defaults: {
+         income: 0,
+         outcome: 0,
+         date: null,
+         transactions: undefined
+      },
+      initialize: function () {
+         this.set('income', this.get('transactions').calcIncome());
+         this.set('outcome', this.get('transactions').calcOutcome());
+         this.listenTo(this.get('transactions'), 'change:amount', this.recalculate)
+         this.listenTo(this.get('transactions'), 'remove', this.subtract)
+      },
+      recalculate: function (transactionModel) {
+         var affectedParam = transactionModel.cases('income', 'outcome');
+         if (affectedParam) {
+            this.set(affectedParam, this.get(affectedParam) + transactionModel.delta());
+         }
+         this.trigger('recalculated:amount');
+      },
+      subtract: function (transactionModel) {
+         var affectedParam = transactionModel.cases('income', 'outcome');
+         if (affectedParam) {
+            this.set(affectedParam, this.get(affectedParam) - transactionModel.get('amount'));
+         }
+         this.trigger('recalculated:amount');
+      }
+   });
 
    var Intervals = Backbone.Collection.extend({
-      url: finstatDefaults.endpoints.transactions,
       model: Interval,
-      dates: {},
+      url: 'api/transactions',
       parse: function (response) {
          var dates = [],
             currentDate = null,
@@ -87,125 +220,33 @@ define([
             currentTransactions.push(new Transaction(thisTransaction));
          }
          return dates;
-      },
-      initialize: function () {
-      }
-   });
-
-   var TransactionView = Backbone.View.extend({
-      className: "row finstat__show-on-hover_area finstat__highlight-row",
-      template: _.template(transactionTpl),
-      dateSplitterTpl: _.template(headerTpl),
-      initialize: function (options) {
-         this.model.bind('change', options.header.render, options.header);
-         this.model.bind('sync', options.render, this);
-      },
-      render: function () {
-         var
-            self = this,
-            values = this.model.toJSON();
-         switch (values.transaction_type) {
-            case single.consts.TT_INCOME:
-               values.rowClass = 'finstat__bar-income';
-               break;
-            case single.consts.TT_OUTCOME:
-               values.rowClass = 'finstat__bar-outcome';
-               break;
-            default:
-               values.rowClass = '';
-         }
-         values.account_from = single.accounts.getName(values.fk_account_from);
-         values.account_to = single.accounts.getName(values.fk_account_to);
-         values.category = single.categories.getName(values.fk_category);
-         this.$el.html(this.template(values));
-         deps.turnEditable.call(this);
-         this.$('.finstat__remove-icon').click(function () {
-            self.model.destroy();
-            self.remove();
-         });
-         return this;
-      }
-   });
-
-   var TransactionsView = Backbone.View.extend({
-      initialize: function (options) {
-         var self = this;
-         this.header = options.header;
-         this.collection.on('add', function (model, collection) {
-            self.createElement.call(self, model)
-         });
-      },
-      render: function () {
-         this.collection.each(function (transaction) {
-            var transactionView = new TransactionView({
-               model: transaction,
-               header: this.header
-            });
-            this.$el.append(transactionView.render().el);
-         }, this);
-         return this;
-      },
-      createElement: function (transaction) {
-         var transactionView = new TransactionView({
-            model: transaction,
-            header: this.header
-         });
-         this.$el.append(transactionView.render().el);
       }
    });
 
    var IntervalHeaderView = Backbone.View.extend({
       className: "row finstat__tall-row finstat__highlight-row",
+      model: Interval,
       template: _.template(headerTpl),
-      render: function () {
-         var
-            params = this.model.toJSON(),
-            income = params.transactions.calcIncome(),
-            outcome = params.transactions.calcOutcome();
-         this.$el.html(this.template({
-            date: params.date,
-            income: income ? '+' + income : '',
-            outcome: outcome || ''
-         }));
-         return this;
-      }
-   });
-
-   var TransactionFormView = Backbone.View.extend({
-      tagName: 'form',
-      id: 'finstat__form-edit-transaction',
-      className: "form-inline",
-      template: _.template(formTpl),
       initialize: function () {
-         this.$el.attr({
-//            action: "/finstat/api/transactions/list",
-//            method: "post"
-         })
+         this.listenTo(this.model, 'recalculated:amount', this.updateStats);
       },
       render: function () {
-         var self = this;
-         this.$el.html(this.template({
-            amount: 100,
-            date: (new Date()).toISOString().substring(0, 10)
-         })).find('.finstat__submit-icon').click(function () {
-            self.model.set(self.getFormData());
-            self.model.save();
-         });
+         this.$el.html(this.template({date: this.model.get('date')}));
+         this.updateStats();
          return this;
       },
-      getFormData: function () {
-         return $('#finstat__form-edit-transaction').serializeArray().reduce(
-            function (attributes, item) {
-               attributes[item.name] = item.value;
-               return attributes;
-            }, {}
-         );
+      updateStats: function () {
+         this.$('.finstat__bar-income').text(this.model.get('income'));
+         this.$('.finstat__bar-outcome').text(this.model.get('outcome'));
       }
    });
 
    var IntervalsView = Backbone.View.extend({
+      events: {
+         'click .finstat__add-icon': 'showForm'
+      },
+      template: _.template(addRow),
       url: 'api/transactions',
-      addRowTemplate: _.template(addRow),
       initialize: function () {
          var self = this;
          if (!this.collection) {
@@ -227,20 +268,11 @@ define([
          });
       },
       render: function () {
-         var intervalsView = this;
-         this.$el.html(this.addRowTemplate()).find('.finstat__add-icon').click(function () {
-            $(this).hide();
-            var form = new TransactionFormView({
-               model: new Transaction({}, {alwaysNew: true})
-            }).render();
-            form.model.on('sync', intervalsView.insertNewItem, intervalsView);
-            intervalsView.$el.prepend(form.el);
-         });
+         this.$el.html(this.template());
          this.collection.each(function (interval) {
             var intervalHeaderView = new IntervalHeaderView({model: interval});
             var transactionsView = new TransactionsView({
-               collection: interval.get('transactions'),
-               header: intervalHeaderView
+               collection: interval.get('transactions')
             });
             this.$el.append(intervalHeaderView.render().el);
             this.$el.append(transactionsView.render().el);
@@ -248,12 +280,25 @@ define([
          this.trigger('rendered');
          return this;
       },
-      insertNewItem: function (transactionModel) {
-         var transactionsCollection = this.getByDate(transactionModel.get('date'));
+      showForm: function () {
+         this.$('.finstat__add-icon').hide();
+         var form = new TransactionFormView({
+            model: new Transaction({}, {alwaysNew: true})
+         }).render();
+         this.listenTo(form.model, 'sync', this.insertNewItem);
+         this.$el.prepend(form.el);
+      },
+      insertNewItem: function (transactionFormModel) {
+         var transactionsCollection = this.getByDate(transactionFormModel.get('date'));
+         transactionFormModel.set(
+            'transaction_type',
+            single.accounts.getOperationType(
+               transactionFormModel.get('fk_account_from'),
+               transactionFormModel.get('fk_account_to')
+            ));
+
          if (transactionsCollection) {
-            console.log('beforeAdd');
-            transactionsCollection.get('transactions').add(transactionModel.clone());
-            console.log('afterAdd');
+            transactionsCollection.get('transactions').add(transactionFormModel.clone());
          }
       },
       getByDate: function (date) {
