c59de4ea56bb257eec944fe6f541306f3e46625c
Gleb Lobastov <0000reg@gmail.com>
2016-09-09 23:50:33 +0500

    Расширение jQuery для навешивания бекбоновского обработчика на ссылки (синтаксис не меняется, остается аттрибут href, но для навигации используется Backbone.history вместо перенаправления)

---
+++ finstat/client/finstat/extensions/navigable/navigable.js
@@ -0,0 +1,18 @@
+define([
+   "jquery",
+   "backbone"
+], function ($, Backbone) {
+   $.fn.extend({
+      navigable: function () {
+         return this.on("click", "a[data-nav]", function (event) {
+            var href = $(this).attr("href");
+            if (href) {
+               event.preventDefault();
+               if (href != Backbone.history.getPath()) {
+                  Backbone.history.navigate(href);
+               }
+            }
+         });
+      }
+   });
+});
